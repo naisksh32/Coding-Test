71b1cb9fedf725a8f4cf613bbac2a4eb1b340245
naisksh32 <naisksh32@gmail.com>
2025-08-28 19:39:08 +0900

    추가문제

---
+++ 14th_challenge/18th_2/solved.py
@@ -0,0 +1,20 @@
+import sys
+
+def fib(n):
+    f[1] = 1
+    f[2] = 1
+
+    for i in range(3, n + 1):
+        f[i] = f[i - 1] + f[i - 2]
+
+    return f[n]
+
+def fibonacci(n):
+    return n - 2
+
+n = int(sys.stdin.readline())
+
+f = [0] * (n + 1)
+
+print(fib(n), end=' ')
+print(fibonacci(n))
