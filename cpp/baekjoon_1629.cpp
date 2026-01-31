bc8ac9a564ee0608f907dc192d1b94039766256e
naisksh32 <naisksh32@gmail.com>
2026-02-01 00:29:59 +0900

    ✅ C++ 재귀 01
    C++ 재귀 01

--- cpp/baekjoon_1629.cpp
+++ cpp/baekjoon_1629.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
+using ll = long long;
 
-int main(){
+
+
+ll POW(ll a, ll b, ll m){
+    if(b==1) return a%m;
+
+    ll val = POW(a, b/2 ,m);
+    val = val*val % m;
+    if(b%2 == 0) return val;
+    
+    return val * a % m;
+}
+
+int main() {
+    ll A, B, C;
+    cin >> A >> B >> C;
+
+    cout << POW(A, B, C);
     
     return 0;
 }
