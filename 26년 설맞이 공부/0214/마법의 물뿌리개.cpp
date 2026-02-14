894b193f6194b36e06d075ec5cf80d0a6b2f0dec
naisksh32 <naisksh32@gmail.com>
2026-02-15 01:09:51 +0900

    ✅ 마법의 물뿌리개 - Greedy 방법으로 해결

--- 26년 설맞이 공부/0214/마법의 물뿌리개.cpp
+++ 26년 설맞이 공부/0214/마법의 물뿌리개.cpp
@@ -1,86 +1,58 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <cstring>
-#include <algorithm>
 using namespace std;
 
-// 나무의 갯수
-int N;
-int trees[101];	// 나무들의 높이
-int max_height;	//최고 나무 높이
-int min_day;	// 최저 날짜
 
-// 초기화 함수
+int N;			// 나무의 갯수
+int trees[101];	// 나무의 높이 정보
+int ans = 0;
+
 void init() {
-	N = 0;
+	N = 0; ans = 0;
 	memset(trees, 0, sizeof(trees));
-	max_height = 0;
-	min_day = 21e8;
 }
-// 입력 함수
 void input() {
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		cin >> trees[i];
-		max_height = max(max_height, trees[i]);
 	}
-}
-
-
-void dfs(int day, bool watered) {
-	// 기저
-	bool flag = true;
-	for (int i = 0; i < N; i++) {
-		if (trees[i] != max_height) {
-			flag = false;
-			break;
-		}
-	}
-	// 모든 나무의 높이가 최고 나무의 높이랑 같다면
-	// 최저 날짜를 반환하고 탈출
-	if (flag) {
-		min_day = min(min_day, day);
-		return;
-	}
-
-	// 성장 크기 설정
-	int grow;
-	if (day % 2 == 0) grow = 1;
-	else grow = 2;
-
-	// 재귀
-	// 각 나무마다 물을 줄지, 안줄지 브랜치 생성
-	for (int tree = 0; tree < N; tree++) {
-		// 가지치기	- 만약 해당 나무가 최대 높이보다 더 성장한다면 pass
-		if (trees[tree]+grow > max_height) continue;
-
-		// 2가지 경우의 수 - 물을 안뿌리거나, 뿌리거나
-		for (int i = 0; i < 2; i++) {
-			if (i == 0 && watered) {
-				dfs(day + 1, false);
-			}
-			else {
-				// 전처리
-				trees[tree] += grow;
-
-				// 재귀함수
-				dfs(day + 1, true);
-
-				// 복귀
-				trees[tree] -= grow;
-
-			}
-		}
-		
-	}
-
 }
 
 void solve() {
-	
-	// 처음엔 안주고 pass 할 수 있으므로
-	dfs(0, true);
+	// 트리의 최고 높이 구하기
+	int max_h = 0;
+	for (int i = 0; i < N; i++) {
+		max_h = max(max_h, trees[i]);
+	}
+
+	int one = 0;	// 필요 1의 갯수 (홀수 날짜)
+	int two = 0;	// 필요 2의 갯수 (짝수 날짜)
+
+	// 각 나무별 필요한 1과 2의 갯수 파악
+	for (int i = 0; i < N; i++) {
+		// 최고 나무 높이에서 현재 나무 높이 빼기
+		int diff = max_h - trees[i];
+		// 만약 차이가 없다면 다음 나무로
+		if (diff == 0) continue;
+
+		two += diff / 2;	// 차이의 2의 몫
+		one += diff % 2;	// 차이의 2의 나머지
+	}
+
+	// 2가 너무 많으면 1, 1로 쪼개서 분배 - Greedy
+	// 1, 2, 1, 2, ...
+	while (two > one + 1) {
+		two -= 1;
+		one += 2;
+	}
+
+	if (one > two) ans = one * 2 - 1;
+	else ans = two * 2;
+
 }
+
 
 int main() {
 	int T;
@@ -90,8 +62,6 @@ int main() {
 		input();
 		solve();
 
-		cout << '#' << t << ' ' << min_day << endl;
+		cout << '#' << t << ' ' << ans << endl;
 	}
-	
-	return 0;
 }
