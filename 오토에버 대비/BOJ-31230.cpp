fdc27fd978c7aab81f6dfdf947a2a6010be3e686
naisksh32 <naisksh32@gmail.com>
2026-03-17 23:16:01 +0900

    ✅ 모비스터디 - 다익스트라로 경로를 검색 & BFS로 노드 역추적

--- 오토에버 대비/BOJ-31230.cpp
+++ 오토에버 대비/BOJ-31230.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int N, M;
@@ -10,7 +11,7 @@ int A, B;
 // 도로 정보를 저장
 struct Edge {
     int to;
-    int cost;
+    long long cost;
 
     // 정렬 방식 - cost가 작은게 우선순위
     bool operator<(Edge right) const {
@@ -25,29 +26,96 @@ struct Edge {
 // 도로 정보를 저장할 인접 리스트
 vector<Edge> alis[300001];
 
-int dist[200001];    // 비용배열
-int history[200001]; // 경로 추적을 위한 배열
+long long dist[200001];    // 비용배열
+vector<int> history[200001]; // 경로 추적을 위한 배열
+vector<int> arr;    // 경로 기록
 
 // 다익스트라로 최단거리 비용 & 경로 계산
 void dijkstra(int start, int end) {
     // dist, history 배열 초기화
+    for (int i = 1; i <= N; i++) {
+        dist[i] = 1e18;
+        history[i].clear();
+    }
+    arr.clear();
+
+    // 초깃값 설정
+    priority_queue<Edge> pq;
+    pq.push({ start, 0 });
+    dist[start] = 0;
+    history[start].push_back(0);
+
+    // 큐가 빌 때까지 진행
+    while (!pq.empty()) {
+        Edge now = pq.top(); pq.pop();
+
+        if (dist[now.to] < now.cost) continue;
+
+        for (int i = 0; i < alis[now.to].size(); i++) {
+            Edge next = alis[now.to][i];
+            long long next_cost = dist[now.to] + next.cost;
+
+            if (dist[next.to] > next_cost) {
+                dist[next.to] = next_cost;
+                history[next.to].clear();
+                history[next.to].push_back(now.to);
+                pq.push({ next.to, next_cost });
+            }
+            else if (dist[next.to] == next_cost) {
+                history[next.to].push_back(now.to);
+            }
+        }
+    }
+
+    // BFS로 최단 경로 찾기
+    bool visited[200001] = { false, };
+    queue<int > q;
+
+    q.push(end);
+    visited[end] = true;
+
+    while (!q.empty()) {
+        int now = q.front(); q.pop();
+        if (now == 0) continue;
+        arr.push_back(now);
+
+        for (int i = 0; i < history[now].size(); i++) {
+            int next = history[now][i];
+
+            if (visited[next]) continue;
+
+            visited[next] = true;
+            q.push(next);
+        }
+    }
+
 }
+
 
 int main() {
     // 주어진 정보 받기
     cin >> N >> M >> A >> B;
 
     // 도로 정보 받기
-    for (int i = 0; i < N; i++) {
-        int from, to, cost;
+    for (int i = 0; i < M; i++) {
+        int from, to;
+        long long cost;
         cin >> from >> to >> cost;
 
         // 양방향 연결
-        alis[from].push_back({to, cost});
-        alis[to].push_back({from, cost});
+        alis[from].push_back({ to, cost });
+        alis[to].push_back({ from, cost });
     }
 
     dijkstra(A, B);
+
+    sort(arr.begin(), arr.end());
+
+    cout << arr.size() << '\n';
+
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << ' ';
+    }
 
     return 0;
 }
