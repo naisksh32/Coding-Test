8d8a2885e6b5bab6c9409b49ca7007797793a52e
naisksh32 <naisksh32@gmail.com>
2026-03-26 00:12:45 +0900

    ✅ 두 도로 - 플루이드워셜을 통한 최단거리 구하기

--- 오토에버 대비/BOJ-26125.cpp
+++ 오토에버 대비/BOJ-26125.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int N, M, S, T;
 int Q;
 
-int roads[301][301];
+long long roads[301][301];
 
 
 void show() {
@@ -18,7 +18,34 @@ void show() {
 	}
 }
 
-void fluid() {
+
+int main() {
+
+	// 입출력 속도 향상
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N >> M >> S >> T;
+
+	// 초기화
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (i == j) roads[i][j] = 0;
+			else roads[i][j] = 1e15;
+		}
+	}
+
+	// 입력
+	for (int i = 0; i < M; i++) {
+		int from, to;
+		long long cost;
+
+		cin >> from >> to >> cost;
+
+		roads[from][to] = min(cost, roads[from][to]);
+	}
+	
+	// 플루이드-워셜
 	for (int k = 1; k <= N; k++) {
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= N; j++) {
@@ -29,41 +56,24 @@ void fluid() {
 			}
 		}
 	}
-}
 
-int main() {
-	cin >> N >> M >> S >> T;
-
-	// 초기화
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			if (i == j) roads[i][j] = 0;
-			else roads[i][j] = 1e9;
-		}
-	}
-
-	// 입력
-	for (int i = 0; i < M; i++) {
-		int from, to, cost;
-		cin >> from >> to >> cost;
-
-		roads[from][to] = cost;
-	}
-	
+	//show();
 
 	cin >> Q;
 	for (int i = 0; i < Q; i++) {
 		int a1, b1, c1, a2, b2, c2;
 		cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
-
-		roads[a1][b1] = c1;
-		roads[a2][b2] = c2;
 		
-		fluid();
-		cout << roads[S][T] << '\n';
+		long long ans = roads[S][T];
 
-		roads[a1][b1] = 1e9;
-		roads[a2][b2] = 1e9;
+		ans = min(ans, roads[S][a1] + c1 + roads[b1][T]);
+		ans = min(ans, roads[S][a2] + c2 + roads[b2][T]);
+		ans = min(ans, roads[S][a1] + c1 + roads[b1][a2] + c2 + roads[b2][T]);
+		ans = min(ans, roads[S][a2] + c2 + roads[b2][a1] + c1 + roads[b1][T]);
+
+		if (ans >= 1e15) cout << -1 << '\n';
+		else cout << ans << '\n';
+
 	}
 
 	return 0;
